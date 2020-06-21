554ce05f14ab1d6c00b6b47d57ec3edb4b3d33cb
Bbhushan1 <bhushan.bharat993@gmail.com>
2020-06-22 01:24:38 +0530

    Modification for Live deployment to aws lambda

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,31 @@ const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 
 module.exports = {
-    entry: slsw.lib.entries,
-    target: 'node',
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    'imports-loader?graphql',
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [['env', { targets: { node: '12.18' } }]],
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.join(__dirname, '.webpack'),
-        filename: '[name].js',
-    },
+	entry: slsw.lib.entries,
+	target: 'node',
+	// externals: [nodeExternals({
+	// 	modulesDir: path.resolve(__dirname, './node_modules'),
+	// })],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					'imports-loader?graphql',
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [['env', { targets: { node: '12.18' } }]],
+						},
+					},
+				],
+			},
+		],
+	},
+	output: {
+		libraryTarget: 'commonjs',
+		path: path.join(__dirname, '.webpack'),
+		filename: '[name].js',
+	},
+	mode: 'development',
 };
