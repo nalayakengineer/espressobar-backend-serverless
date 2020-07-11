8d5cfb4fabd5a1715ecd5c5b0cd706d757099cc9
Bbhushan1 <bhushan.bharat993@gmail.com>
2020-07-11 18:41:55 +0530

    Added DynamodDB database

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -15,40 +15,6 @@ const item = (_, args) => {
 }
 
 
-// add to handler.js
-// const promisify = foo =>
-//     new Promise((resolve, reject) => {
-//         foo((error, result) => {
-//             if (error) {
-//                 reject(error);
-//             } else {
-//                 resolve(result);
-//             }
-//         });
-//     });
-
-
-// const data = {
-//     getitems(handle, args) {
-//         return promisify(callback => {
-//             callback
-//         }).then(result => {
-//             return menu
-//         })
-//     },
-//     getitem(handle, args) {
-//         return promisify(callback => { callback }).then(result => {
-//             if (args.type) {
-//                 return menu.filter(item => item.type === args.type)
-//             }
-//             if (args.id) {
-//                 return menu.filter(item => item.id === args.id)
-//             }
-//         })
-//     }
-// }
-
-
 
 export const resolvers = {
     Query: {
